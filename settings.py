29f40d11a665e67958796579537e74fd5bd56f82
Killswitch5150 <92342531+Killswitch5150@users.noreply.github.com>
2024-10-26 03:32:50 -0400

    -fixed ui bug (ammo/kill count overlap) -moved ammo count to below health -added wave display ui element -implemented new wave system every 10 kills (max 10) -changed spawning timers for enemies and ammo drops -fixed bug that made fullscreen toggle acts as "Quit" button in game over screen -removed unused code -cleaned up code

--- settings.py
+++ settings.py
@@ -19,13 +19,19 @@ class GameSettings:
         self.ammo_drop_spawn_interval = 10000
         self.max_ammo_boxes = 2
         self.kill_count = 0
-        self.current_wave = 0
-        
+        self.current_wave = 1
+
         #wave settings
         self.waves = [
             {'enemy_spawn_rate': 1000, 'ammo_spawn_rate': 15000},
             {'enemy_spawn_rate': 800, 'ammo_spawn_rate': 16000},
             {'enemy_spawn_rate': 500, 'ammo_spawn_rate': 17500},
-            {'enemy_spawn_rate': 250, 'ammo_spawn_rate': 20000}
+            {'enemy_spawn_rate': 250, 'ammo_spawn_rate': 20000},
+            {'enemy_spawn_rate': 200, 'ammo_spawn_rate': 22500},
+            {'enemy_spawn_rate': 150, 'ammo_spawn_rate': 25000},
+            {'enemy_spawn_rate': 100, 'ammo_spawn_rate': 27500},
+            {'enemy_spawn_rate': 75, 'ammo_spawn_rate': 30000},
+            {'enemy_spawn_rate': 50, 'ammo_spawn_rate': 32500},
+            {'enemy_spawn_rate': 25, 'ammo_spawn_rate': 35000}
         ]
         
