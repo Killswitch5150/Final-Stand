fff61fe98290429681ebd8986f947fe04858621c
Killswitch5150 <92342531+Killswitch5150@users.noreply.github.com>
2024-10-31 11:42:35 -0400

    -adjusted pygame clock method placement -tweaked amounts of ammo in ammo drops -adjusted some logic comments -created weighted probabilities for ammo drop quantities -ensured audio can not attempt to play when game is closing -laid groundwork for volume control of sounds -created sound control class to be implemented later

--- sounds.py
+++ sounds.py
@@ -1,4 +1,14 @@
 import pygame, variables #variables is where sound support is initialized
 
+default_volume = 0.5
+
 shooting = pygame.mixer.Sound("sounds/shooting.wav")
 reloading = pygame.mixer.Sound("sounds/reload.wav")
+
+#class soundcontrol(self, sound):
+#    def __init__(self)
+#        self.volume = 0.5
+    
+#    def soundfiles(sound):
+#        shooting = pygame.mixer.Sound("sounds/shooting.wav")
+#        reloading = pygame.mixer.Sound("sounds/reload.wav")
