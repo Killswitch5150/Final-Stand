04d4635801aa1e3619f21786c77601e174bb1b21
Killswitch5150 <92342531+Killswitch5150@users.noreply.github.com>
2024-10-31 08:35:43 -0400

    -cleaned up importing system -tweaked ammo spawn rates to increase difficulty -removed deprecated code -added sound module to load sounds -added sound functions to main.py -implemented shooting and reloading sounds and functions

---
+++ sounds.py
@@ -0,0 +1,4 @@
+import pygame, variables #variables is where sound support is initialized
+
+shooting = pygame.mixer.Sound("sounds/shooting.wav")
+reloading = pygame.mixer.Sound("sounds/reload.wav")
