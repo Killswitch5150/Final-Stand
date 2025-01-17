9db6d0aea327d588dae1c8c6add9248d62a8da62
Killswitch5150 <92342531+Killswitch5150@users.noreply.github.com>
2025-01-16 19:14:49 -0500

    -modified splash_fls.py and splash_pygame.py to make as easy to modify as possible. splash screens can now be edited by only changing 4 easy to understand variables. more advanced users can skim through the code to change other aspects of splash screens, but more basic users can easily jump in to the project and use the code by adjusting variables to make changes to splash screens.

--- splash/splash_fls.py
+++ splash/splash_fls.py
@@ -1,11 +1,32 @@
 import pygame
 import time  #for delay
 
+#--customization--
+# these are the only variables needed to change splash screen display properties
+
+splash_screen_path = "./splash/img/frontlinestudios_logo_1080.png"
+#provide string literal to image you want to be displayed on the splash screen
+#default is: "./splash/img/frontlinestudios_logo_1080.png"
+
+splash_screen_scale_factor = 0.4
+#provide float value for scale of the original image file to fit the splash screen
+#default is: 0.4
+
+splash_screen_timer_duration = 3000
+#provide integer value for duration of how long the splash screen should be shown
+#default is: 3000 (3 seconds)
+
+splash_img_pos = (25, 80)
+#provide tuple value for position of image to be displayed
+#default is (25, 80)
+
+#--
+
 def load_and_scale_image(path, scale_factor):  
     image = pygame.image.load(path).convert_alpha() 
     return pygame.transform.scale(image, (int(image.get_width() * scale_factor), int(image.get_height() * scale_factor)))  # Return scaled image
 
-def run_splash_screen(image_path="./splash/img/frontlinestudios_logo_1080.png", scale_factor=0.4, timer_duration=3000):
+def run_splash_screen(image_path=splash_screen_path, scale_factor=splash_screen_scale_factor, timer_duration=splash_screen_timer_duration):
     pygame.init()
 
     game_resolution = (800, 600)
@@ -31,6 +52,6 @@ def run_splash_screen(image_path="./splash/img/frontlinestudios_logo_1080.png", 
 
         
         window.fill((0, 0, 0))  #black background
-        window.blit(image, (25, 80))  #draw image
+        window.blit(image, splash_img_pos)  #draw image
         
         pygame.display.update()
